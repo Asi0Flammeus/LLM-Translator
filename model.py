b50128c580c12e56183b381aceaac25816d6850f
Asi0Flammeus <asi0.flammeus@pm.me>
2023-04-09 14:33:13 +0200

    multiple chunks and lecture draft

--- model.py
+++ model.py
@@ -22,6 +22,21 @@ class TranscriptionModel:
         self.audio_files = []
         self.transcript_files = []
 
+    # Define constants
+    MODEL_ENGINE = "gpt-3.5-turbo"
+    AUDIO_EXTENSIONS = ('.wav', '.mp3')
+    MAX_AUDIO_SIZE_MB = 20  # (a bit smaller than the) Maximum audio size supported by Whisper API in MB
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+
+    def __init__(self):
+        """
+        Initializes the TranscriptionModel instance with the OpenAI API key.
+        """
+        self.transcript = None
+        self.formatted_transcript = []
+        self.audio_files = []
+        self.transcript_files = []
+
     def load_audio(self, file_path):
         """
         Loads an audio file from the specified file path.
@@ -35,12 +50,28 @@ class TranscriptionModel:
         if not os.path.isfile(file_path) or not file_path.endswith(self.AUDIO_EXTENSIONS):
             raise ValueError(f"Invalid audio file: {file_path}")
 
-        self.audio_files.append(file_path)
+        # Check the size of the audio file
+        audio_size_mb = os.path.getsize(file_path) / (1024 * 1024)
+        if audio_size_mb > self.MAX_AUDIO_SIZE_MB:
+            # Split the audio file into chunks of MaximumSizeFile MB
+            chunk_size_mb = self.MAX_AUDIO_SIZE_MB
+            with open(file_path, "rb") as f:
+                chunk = f.read(chunk_size_mb * 1024 * 1024)
+                i = 0
+                while chunk:
+                    chunk_file_path = f"{os.path.splitext(file_path)[0]}_{i}{os.path.splitext(file_path)[1]}"
+                    print(chunk_file_path)
+                    with open(chunk_file_path, "wb") as chunk_file:
+                        chunk_file.write(chunk)
+                    self.audio_files.append(chunk_file_path)
+                    i += 1
+                    chunk = f.read(chunk_size_mb * 1024 * 1024)
+        else:
+            self.audio_files.append(file_path)
 
     def transcribe_audio(self):
             """
             Transcribes the audio data using OpenAI's Whisper API.
-
             Returns:
                 str: The transcript of the audio data in text format.
             """
@@ -65,10 +96,30 @@ class TranscriptionModel:
 
             # Extract the transcript text
             transcript_text = transcript["text"]
-            self.transcript = transcript_text
 
             return transcript_text
 
+    def transcribe_multiple_chunks_audio(self):
+        """
+        Transcribes all the audio chunks into a single transcript
+
+        Returns:
+            str: The transcript of the audio data in text format.
+        """
+        if not self.audio_files:
+            raise ValueError("No audio files have been loaded.")
+
+        # Transcribe each audio file
+        transcript_texts = []
+        while self.audio_files:
+            transcript_text = transcribe_audio
+            transcript_texts.append(transcript_text)
+            self.audio_files.pop(0)
+
+        # Concatenate the transcript texts into a single string
+        self.transcript = " ".join(transcript_texts)
+
+        return self.transcript
 
     def save_transcript(self):
         """
@@ -167,4 +218,16 @@ class TranscriptionModel:
 
         prompt=f"Translate the following transcript to {language}:\n\n"
         suffix=f"{language}_transcript_translation"
+        self.save_manipulated_text(self.manipulate_text(prompt),suffix)
+
+    def write_synthetic_lecture(self):
+        """
+        writes a synthetic lecture that would be based on the transcript text in a markdown format.
+
+        Returns:
+            str: The transcript formatted into a synthetic lecture.
+        """
+
+        prompt=f"Reformulate the following transcript into a synthetic and structured lecture in markdown format:"
+        suffix=f"cours_academic_FR"
         self.save_manipulated_text(self.manipulate_text(prompt),suffix)
