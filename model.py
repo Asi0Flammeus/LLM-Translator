c740fb3f964c51e24e428b69c7f083fb53c8b100
Asi0Flammeus <asi0.flammeus@pm.me>
2024-02-18 15:13:14 +0100

    modify chatgpt model

--- model.py
+++ model.py
@@ -15,7 +15,7 @@ class OpenaiTranslationModel:
 
         load_dotenv()
         openai.api_key = os.getenv("OPENAI_API_KEY")
-        self.model_engine = "gpt-3.5-turbo"
+        self.model_engine = "gpt-4-0125-preview"
         self.error_handler = APIErrorHandler()
 
         self.prompt = ""
